3875e66845f0182ad175a2bcd8244bd09b0fe1e7
aitzeng <aitzeng@gmail.com>
2024-01-23 21:37:02 -0800

    Updated text color

--- components/CreateForm.js
+++ components/CreateForm.js
@@ -37,15 +37,15 @@ const CreateForm = () => {
     <div className="h-screen items-center justify-center flex">
       <form className="">
         <div className="email-parent mx-2 my-2">
-          <p className="font-bold">Email:</p>
+          <p className="font-bold text-black">Email:</p>
           <input value={email} onChange={handleEmailChange} placeholder="john.doe@example.com" className="border-2 border-gray-300 rounded-md" />
         </div>
         <div className="github-url-parent mx-2 my-2">
-          <p className="font-bold">Github Repo URL:</p>
+          <p className="font-bold text-black">Github Repo URL:</p>
           <input onChange={handleGithubURLChange} placeholder="github.com/username/example" className="border-2 border-gray-300 rounded-md"/>
         </div>
         {submitStatus === 1 ? <p className="mx-2 my-2 text-red-500">Submission successful</p> : submitStatus === 2 ? <p className="mx-2 my-2 text-red-500">Error submitting</p> : null}
-        <button onClick={submitForm} className="mx-2 my-2 px-2 border-2 border-gray-500 bg-gray-200" type="submit">Submit</button>
+        <button onClick={submitForm} className="mx-2 my-2 px-2 text-black border-2" type="submit">Submit</button>
       </form>
     </div>
   )
